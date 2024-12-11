038131b4914bb486c55609b86dcd9ee8ad3ad7dd
Jennifer Pichardo <jenniferpichardo15@gmail.com>
2024-12-10 20:45:34 -0500

    Implement 404 page

--- server/router.js
+++ server/router.js
@@ -15,6 +15,11 @@ const router = (app) => {
   app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+
+  // 404 page
+  app.use((req, res) => {
+    res.status(404).render('404');
+  });
 };
 
 module.exports = router;
